1b39ceb856e1a28a3ee17c0ab8097aa686b45ae1
Guillaume PHP <123957068+guillaumelfn@users.noreply.github.com>
2023-02-02 16:15:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ require_once 'OpenAI.php';
 
 $openai = New OpenAI();
 
-// Please configure parameters of temperature, top p, frequency and presence penalty, best of, in includes/openAI.php, function configuration
+// Please configure parameters of temperature, top p, frequency and presence penalty, best of, in openAI.php, function configuration
 // This is the prompt..
 
 $question = "You are an ultimate god robot and you can answer any question. After the answer, no other question can be asked.
 Q:What is the meaning of life ?
 A:";
 
-// LEt's get a response.
+// Let's get a response.
 // Usage : completion(model,start of question, length);
 $response = $openai->completion("text-davinci-003", $question, 256);
 
